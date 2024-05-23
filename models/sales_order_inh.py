b3e67bae16f17d6bec55a4d886f352c6822d431c
CyberRoms <romualdojrb@yahoo.com>
2024-05-23 14:43:17 +0400

    initial

--- models/sales_order_inh.py
+++ models/sales_order_inh.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class sales_order_inh(models.Model):
     _inherit = 'sale.order'
 
-    project_reference = fields.Many2one('project.project', string='Project Reference')
-    project_status = fields.Selection(related='project_reference.project_status')
+    inh_project_reference = fields.Many2one('project.project', string='Project Reference')
+    inh_project_status = fields.Selection(related='inh_project_reference.inh_project_status')
 
 
