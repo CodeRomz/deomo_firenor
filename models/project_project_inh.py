3d7ff3ff4024df2c12b8e1c0ae1ddfe4a4d87116
CyberRoms <romualdojrb@yahoo.com>
2024-05-23 13:35:14 +0400

    initial

--- models/project_project_inh.py
+++ models/project_project_inh.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class project_project_inh(models.Model):
     _inherit = 'project.project'
 
-    project_status = fields.Selection([
+    inh_project_status = fields.Selection([
         ('active', 'Active'),
         ('closed', 'Closed'),
     ], string='Project Status',
